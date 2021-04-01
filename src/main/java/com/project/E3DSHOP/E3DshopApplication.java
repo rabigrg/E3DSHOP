1ddfaa13bc59305de7482dde7862d1ce00c17088
Rabi Gurung <44107376+rabigrg@users.noreply.github.com>
2021-03-31 20:13:56 -0500

    initial project setup

---
+++ src/main/java/com/project/E3DSHOP/E3DshopApplication.java
@@ -0,0 +1,13 @@
+package com.project.E3DSHOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E3DshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E3DshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/E3DSHOP/E3DshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.E3DSHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E3DshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
